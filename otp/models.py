9dbf70dcdc975ca3a7bdad36e00c3eff41268cc6
Yubraj Sigdel <yubraj.sigdel@lisnepal.com.np>
2023-02-07 14:24:15 +0545

    Added API for otp validiation based on time limit

--- otp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Otp(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    email = models.CharField(max_length=255)
-    otp = models.CharField(max_length=4)
-
